905d2dd3a55f3c630394d3b2183ab47d1710aa2b
Marko <justmarkoven@gmail.com>
2024-04-19 16:24:47 +0800

    Initial commit

---
+++ src/client/java/io/github/metaconscious/jitchat/JitChatClient.java
@@ -0,0 +1,10 @@
+package io.github.metaconscious.jitchat;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JitChatClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
